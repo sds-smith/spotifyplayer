e3e07ae94911bcfa66d438c7e321298a98802a9d
Shawn Smith <sds.smith24@gmail.com>
2022-05-09 05:46:41 -0400

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Spotify from './Spotify.js'
 import UserProfile from './UserProfile/UserProfile';
 import ProfilePic from './icons/default_profile96.png'
