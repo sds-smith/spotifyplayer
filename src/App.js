a0e9ab4f908c181fd498c6f70753eda2df70f4bb
Shawn Smith <sds.smith24@gmail.com>
2022-05-09 07:35:44 -0400

    add Router in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Spotify from './Spotify.js'
 import Login from './Login.js';
 import UserProfile from './UserProfile/UserProfile';
@@ -37,7 +37,7 @@ function App() {
     // }, [])
 
   return (
-    <Routes >
+    <Router >
         <Route >
           <Login login={login} />
         </Route>
@@ -49,7 +49,7 @@ function App() {
             userName={userName}
           /> 
         </Route>
-    </Routes>
+    </Router>
 
   );
 }
