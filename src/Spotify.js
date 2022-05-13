db8a8acfdcb492d5f4298a66d3cd94d3da119147
Shawn Smith <sds.smith24@gmail.com>
2022-05-13 15:04:48 -0400

    remove unused variables

--- src/Spotify.js
+++ src/Spotify.js
@@ -2,10 +2,8 @@ import { base64urlencode } from './random'
 
 let authCode
 let accessToken
-const codeVerifier = process.env.REACT_APP_AUTH_VERIFIER
 const codeChallenge = process.env.REACT_APP_AUTH_CHALLENGE
 const clientId = process.env.REACT_APP_CLIENT_ID
-const clientSecret = process.env.REACT_APP_CLIENT_SECRET
 const state = process.env.REACT_APP_AUTH_STATE
 // const redirectURI = process.env.REACT_APP_REDIRECT_URI_LOCALHOST
 const redirectURI = process.env.REACT_APP_REDIRECT_URI_NETLIFY_CLI
