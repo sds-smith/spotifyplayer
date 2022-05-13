1d11dc5ac9c121c20d678372ed0ddf409f61b027
Shawn Smith <sds.smith24@gmail.com>
2022-05-13 15:57:17 -0400

    add redirect for /authorize

--- src/Spotify.js
+++ src/Spotify.js
@@ -42,7 +42,7 @@ const Spotify = {
             authCode = this.parseWindow()
             return authCode                  
         } else {
-            window.location.replace(`https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${scope}&state=${state}&code_challenge=${codeChallenge}&code_challenge_method=S256&show_dialog=false&redirect_uri=${redirectURI}`)
+            window.location.replace('/authorize')
         }  
     },
 
